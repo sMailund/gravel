8e8a9912f07c58c9fbd4022cbc6078c189f9eb87
Simen Mailund Svendsen <sima@netlight.com>
2022-12-26 17:52:13 +0100

    move out search functionality

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -37,63 +35,6 @@ func getCommand(args []string) Command {
 	}
 
 	return &UsageCommand{}
-}
-
-func search(term string) {
-	e := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		searchFile(path, term)
-		return nil
-	})
-	if e != nil {
-		log.Fatal(e)
-	}
-}
-
-func searchFile(path string, term string) {
-	f, err := os.Open(path)
-	if err != nil {
-		panic("")
-	}
-	defer f.Close()
-
-	fileInfo, err := f.Stat()
-	if err != nil {
-		panic("")
-	}
-
-	if fileInfo.IsDir() {
-		return
-	}
-
-	scanner := bufio.NewScanner(f)
-
-	matches := []match{}
-
-	line := 1
-	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.Contains(text, term) {
-			match := match{
-				text:       text,
-				lineNumber: line,
-			}
-			matches = append(matches, match)
-		}
-
-		line++
-	}
-
-	printMatches(fileInfo, matches)
-}
-
-func printMatches(fileInfo os.FileInfo, matches []match) {
-	if len(matches) > 0 {
-		fmt.Printf("%v\n", fileInfo.Name())
-		for _, match := range matches {
-			fmt.Printf("%v -> %v\n", match.lineNumber, match.text)
-		}
-		fmt.Println()
-	}
 }
 
 func findLinks(path string) {
--- searchCommand.go
+++ searchCommand.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type SearchCommand struct {
 	searchTerm string
 }
@@ -18,4 +27,61 @@ func (c *SearchCommand) description() string {
 
 func (c *SearchCommand) execute() {
 	search(c.searchTerm)
+}
+
+func search(term string) {
+	e := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		searchFile(path, term)
+		return nil
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func searchFile(path string, term string) {
+	f, err := os.Open(path)
+	if err != nil {
+		panic("")
+	}
+	defer f.Close()
+
+	fileInfo, err := f.Stat()
+	if err != nil {
+		panic("")
+	}
+
+	if fileInfo.IsDir() {
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	matches := []match{}
+
+	line := 1
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.Contains(text, term) {
+			match := match{
+				text:       text,
+				lineNumber: line,
+			}
+			matches = append(matches, match)
+		}
+
+		line++
+	}
+
+	printMatches(fileInfo, matches)
+}
+
+func printMatches(fileInfo os.FileInfo, matches []match) {
+	if len(matches) > 0 {
+		fmt.Printf("%v\n", fileInfo.Name())
+		for _, match := range matches {
+			fmt.Printf("%v -> %v\n", match.lineNumber, match.text)
+		}
+		fmt.Println()
+	}
 }
