4d7824b908eb01828d54d61365de873c6a6cc895
Simen Mailund Svendsen <sima@netlight.com>
2022-12-26 17:52:46 +0100

    move out find links functionality

--- linksCommand.go
+++ linksCommand.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
 type LinksCommand struct {
 	path string
 }
@@ -18,4 +25,43 @@ func (c *LinksCommand) description() string {
 
 func (c *LinksCommand) execute() {
 	findLinks(c.path)
+}
+
+func findLinks(path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		panic("")
+	}
+	defer f.Close()
+
+	linkRegex := regexp.MustCompile("\\[\\S+\\]")
+
+	var links []string
+	scanner := bufio.NewScanner(f)
+	line := 1
+	for scanner.Scan() {
+		text := scanner.Text()
+		matches := linkRegex.FindAllString(text, -1)
+		links = append(links, matches...)
+
+		line++
+	}
+
+	links = removeDuplicateStr(links)
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+}
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -35,45 +33,6 @@ func getCommand(args []string) Command {
 	}
 
 	return &UsageCommand{}
-}
-
-func findLinks(path string) {
-	f, err := os.Open(path)
-	if err != nil {
-		panic("")
-	}
-	defer f.Close()
-
-	linkRegex := regexp.MustCompile("\\[\\S+\\]")
-
-	var links []string
-	scanner := bufio.NewScanner(f)
-	line := 1
-	for scanner.Scan() {
-		text := scanner.Text()
-		matches := linkRegex.FindAllString(text, -1)
-		links = append(links, matches...)
-
-		line++
-	}
-
-	links = removeDuplicateStr(links)
-
-	for _, link := range links {
-		fmt.Println(link)
-	}
-}
-
-func removeDuplicateStr(strSlice []string) []string {
-	allKeys := make(map[string]bool)
-	list := []string{}
-	for _, item := range strSlice {
-		if _, value := allKeys[item]; !value {
-			allKeys[item] = true
-			list = append(list, item)
-		}
-	}
-	return list
 }
 
 type match struct {
