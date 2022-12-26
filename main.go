dc3dc46a625fc7ab848d1d01945b755655f44fc3
Simen Mailund Svendsen <sima@netlight.com>
2022-12-26 17:47:32 +0100

    break out searchcommand

--- main.go
+++ main.go
@@ -140,26 +140,6 @@ type match struct {
 	lineNumber int
 }
 
-type SearchCommand struct {
-	searchTerm string
-}
-
-func (c *SearchCommand) create(args []string) Command {
-	return &SearchCommand{searchTerm: args[1]}
-}
-
-func (c *SearchCommand) flag() string {
-	return "-s"
-}
-
-func (c *SearchCommand) description() string {
-	return "search for a file containing search term"
-}
-
-func (c *SearchCommand) execute() {
-	search(c.searchTerm)
-}
-
 type UsageCommand struct {
 }
 
--- searchCommand.go
+++ searchCommand.go
@@ -1,1 +1,21 @@
 package main
+
+type SearchCommand struct {
+	searchTerm string
+}
+
+func (c *SearchCommand) create(args []string) Command {
+	return &SearchCommand{searchTerm: args[1]}
+}
+
+func (c *SearchCommand) flag() string {
+	return "-s"
+}
+
+func (c *SearchCommand) description() string {
+	return "search for a file containing search term"
+}
+
+func (c *SearchCommand) execute() {
+	search(c.searchTerm)
+}
